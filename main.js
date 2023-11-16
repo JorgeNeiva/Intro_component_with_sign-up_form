a18ae1073d9a87c8ffaa1adb0b86b677303f5d3e
Jorge Neiva <jorge_neiva_@hotmail.com>
2023-11-16 16:44:01 +0000

    JS's conclusion

--- main.js
+++ main.js
@@ -3,11 +3,23 @@ const inputs = document.querySelectorAll('.right-col form input');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    inputs.forEach(input=>{
+    inputs.forEach((input) => {
         if (!input.value) {
             input.parentElement.classList.add('error');
         }else{
-            input.parentElement.classList.remove('error') 
+            input.parentElement.classList.remove('error');
+            if (input.type == 'email') {
+                if (validateEmail(input.value)) {
+                    input.parentElement.classList.remove('error');
+                } else {
+                    input.parentElement.classList.add('error');
+                }
+            }
         }
      });
 });
+
+function validateEmail (email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
